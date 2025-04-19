30db54a766cc8359affa7e5eb0b0dd4f9b8fef99
Grady Phillips <grady@unyx.io>
2025-04-19 18:31:18 +0000

    do inc changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { DISCORD_TOKEN } = require("./config.json");
 const { getEvents } = require("./lib/raid-helper/get-events");
 const { getEvent } = require("./lib/raid-helper/get-event");
 const { getRaidplan } = require("./lib/raid-helper/get-raidplan");
+const { generateAccessToken } = require("./lib/warcraftlogs/");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -46,4 +47,5 @@ for (const file of eventFiles) {
 
 client.login(DISCORD_TOKEN).then(async () => {
   // console.log(await getRaidplan("1361195193340264588"));
+  console.log(generateAccessToken());
 });
